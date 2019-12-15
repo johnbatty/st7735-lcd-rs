b2776791546c6e76fafbd387b71a3f9b0fedc894
Jacob <jacobrosenthal@gmail.com>
2019-12-15 12:04:05 -0700

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,15 @@ pub mod instruction;
 use core::mem::transmute;
 
 use crate::instruction::Instruction;
-use num_traits::ToPrimitive;
 use num_derive::ToPrimitive;
+use num_traits::ToPrimitive;
 
-use embedded_hal::digital::v2::OutputPin;
-use embedded_hal::blocking::spi;
 use embedded_hal::blocking::delay::DelayMs;
+use embedded_hal::blocking::spi;
+use embedded_hal::digital::v2::OutputPin;
 
 /// ST7735 driver to connect to TFT displays.
-pub struct ST7735 <SPI, DC, RST>
+pub struct ST7735<SPI, DC, RST>
 where
     SPI: spi::Write<u8>,
     DC: OutputPin,
@@ -57,14 +57,7 @@ where
     RST: OutputPin,
 {
     /// Creates a new driver instance that uses hardware SPI.
-    pub fn new(
-        spi: SPI,
-        dc: DC,
-        rst: RST,
-        rgb: bool,
-        inverted: bool,
-    ) -> Self
-    {
+    pub fn new(spi: SPI, dc: DC, rst: RST, rgb: bool, inverted: bool) -> Self {
         let display = ST7735 {
             spi,
             dc,
@@ -72,7 +65,7 @@ where
             rgb,
             inverted,
             dx: 0,
-            dy: 0
+            dy: 0,
         };
 
         display
@@ -80,7 +73,8 @@ where
 
     /// Runs commands to initialize the display.
     pub fn init<DELAY>(&mut self, delay: &mut DELAY) -> Result<(), ()>
-        where DELAY: DelayMs<u8>
+    where
+        DELAY: DelayMs<u8>,
     {
         self.hard_reset()?;
         self.write_command(Instruction::SWRESET, None)?;
@@ -89,8 +83,10 @@ where
         delay.delay_ms(200);
         self.write_command(Instruction::FRMCTR1, Some(&[0x01, 0x2C, 0x2D]))?;
         self.write_command(Instruction::FRMCTR2, Some(&[0x01, 0x2C, 0x2D]))?;
-        self.write_command(Instruction::FRMCTR3,
-            Some(&[0x01, 0x2C, 0x2D, 0x01, 0x2C, 0x2D]))?;
+        self.write_command(
+            Instruction::FRMCTR3,
+            Some(&[0x01, 0x2C, 0x2D, 0x01, 0x2C, 0x2D]),
+        )?;
         self.write_command(Instruction::INVCTR, Some(&[0x07]))?;
         self.write_command(Instruction::PWCTR1, Some(&[0xA2, 0x02, 0x84]))?;
         self.write_command(Instruction::PWCTR2, Some(&[0xC5]))?;
@@ -114,8 +110,7 @@ where
         Ok(())
     }
 
-    pub fn hard_reset(&mut self) -> Result<(), ()>
-    {
+    pub fn hard_reset(&mut self) -> Result<(), ()> {
         self.rst.set_high().map_err(|_| ())?;
         self.rst.set_low().map_err(|_| ())?;
         self.rst.set_high().map_err(|_| ())
@@ -123,7 +118,9 @@ where
 
     fn write_command(&mut self, command: Instruction, params: Option<&[u8]>) -> Result<(), ()> {
         self.dc.set_low().map_err(|_| ())?;
-        self.spi.write(&[command.to_u8().unwrap()]).map_err(|_| ())?;
+        self.spi
+            .write(&[command.to_u8().unwrap()])
+            .map_err(|_| ())?;
         if params.is_some() {
             self.write_data(params.unwrap())?;
         }
@@ -143,13 +140,12 @@ where
 
     pub fn set_orientation(&mut self, orientation: &Orientation) -> Result<(), ()> {
         if self.rgb {
-            self.write_command(
-                Instruction::MADCTL, Some(&[orientation.to_u8().unwrap()]
-            ))?;
+            self.write_command(Instruction::MADCTL, Some(&[orientation.to_u8().unwrap()]))?;
         } else {
             self.write_command(
-                Instruction::MADCTL, Some(&[orientation.to_u8().unwrap() | 0x08 ]
-            ))?;
+                Instruction::MADCTL,
+                Some(&[orientation.to_u8().unwrap() | 0x08]),
+            )?;
         }
         Ok(())
     }
@@ -171,14 +167,14 @@ where
     }
 
     /// Sets a pixel color at the given coords.
-    pub fn set_pixel(&mut self, x: u16, y: u16, color: u16) -> Result <(), ()> {
+    pub fn set_pixel(&mut self, x: u16, y: u16, color: u16) -> Result<(), ()> {
         self.set_address_window(x, y, x, y)?;
         self.write_command(Instruction::RAMWR, None)?;
         self.write_word(color)
     }
 
     /// Writes pixel colors sequentially into the current drawing window
-    pub fn write_pixels<P: IntoIterator<Item = u16>>(&mut self, colors: P) -> Result <(), ()> {
+    pub fn write_pixels<P: IntoIterator<Item = u16>>(&mut self, colors: P) -> Result<(), ()> {
         self.write_command(Instruction::RAMWR, None)?;
         for color in colors {
             self.write_word(color)?;
@@ -187,17 +183,27 @@ where
     }
 
     /// Sets pixel colors at the given drawing window
-    pub fn set_pixels<P: IntoIterator<Item = u16>>(&mut self, sx: u16, sy: u16, ex: u16, ey: u16, colors: P) -> Result <(), ()> {
+    pub fn set_pixels<P: IntoIterator<Item = u16>>(
+        &mut self,
+        sx: u16,
+        sy: u16,
+        ex: u16,
+        ey: u16,
+        colors: P,
+    ) -> Result<(), ()> {
         self.set_address_window(sx, sy, ex, ey)?;
         self.write_pixels(colors)
     }
 }
 
-
 #[cfg(feature = "graphics")]
 extern crate embedded_graphics;
 #[cfg(feature = "graphics")]
-use self::embedded_graphics::{drawable::{Pixel, Dimensions}, pixelcolor::Rgb565, Drawing, SizedDrawing};
+use self::embedded_graphics::{
+    drawable::{Dimensions, Pixel},
+    pixelcolor::Rgb565,
+    Drawing, SizedDrawing,
+};
 
 #[cfg(feature = "graphics")]
 impl<SPI, DC, RST> Drawing<Rgb565> for ST7735<SPI, DC, RST>
@@ -211,7 +217,8 @@ where
         T: IntoIterator<Item = Pixel<Rgb565>>,
     {
         for Pixel(coord, color) in item_pixels {
-            self.set_pixel(coord.0 as u16, coord.1 as u16, color.0).expect("pixel write failed");
+            self.set_pixel(coord.0 as u16, coord.1 as u16, color.0)
+                .expect("pixel write failed");
         }
     }
 }
@@ -231,8 +238,13 @@ where
         let top_left = item_pixels.top_left();
         let bottom_right = item_pixels.bottom_right();
 
-        self.set_pixels(top_left.0 as u16, top_left.1 as u16,
-                        bottom_right.0 as u16, bottom_right.1 as u16,
-                        item_pixels.into_iter().map(|Pixel(_coord, color)| color.0)).expect("pixels write failed")
+        self.set_pixels(
+            top_left.0 as u16,
+            top_left.1 as u16,
+            bottom_right.0 as u16,
+            bottom_right.1 as u16,
+            item_pixels.into_iter().map(|Pixel(_coord, color)| color.0),
+        )
+        .expect("pixels write failed")
     }
 }
